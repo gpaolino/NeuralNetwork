08d6b3a4a9e5eb055a80df854ab28cffccf7f070
gpaolino <gp.guglielminopaolo@me.com>
2025-11-04 17:00:11 +0100

    Minor fix and code comments.

--- src/numstruct.c
+++ src/numstruct.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include "numstruct.h"
 #include "utils.h"
 
--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
+
 #include "utils.h"
 
+// Generates a random floating-point number within a specified range.
 float rand_float_range(float low, float high) {
     return low + ((float)rand() / (float)RAND_MAX) * (high - low);
 }
